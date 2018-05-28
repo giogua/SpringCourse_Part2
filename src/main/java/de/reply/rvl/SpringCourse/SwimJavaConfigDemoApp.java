be7aea9d84a5a1e8b3e54f10126b103f85d9012a
Gioele <gioeleguardabassi@gmail.com>
2018-05-28 10:49:03 +0200

    Final commit

--- src/main/java/de/reply/rvl/SpringCourse/SwimJavaConfigDemoApp.java
+++ src/main/java/de/reply/rvl/SpringCourse/SwimJavaConfigDemoApp.java
@@ -9,13 +9,16 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//Get bean from the container
-		Coach theCoach = context.getBean("swimCoach",Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach",SwimCoach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println("email: "+theCoach.getEmail());
+		System.out.println("team: "+theCoach.getTeam());
 
 		//close the context
 		context.close();
